6d11f8e3797dc600ad74ac157d8df3cf2b56ce54
Andrey Dolya <dolya7kk@gmail.com>
2021-04-05 23:01:06 +0300

    build fixes

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // "dbURL": "mongodb://localhost:27017",
+    //&w=majorit
+    // "dbURL": "mongodb+srv://harel:hareldev$@cluster0.sxgml.mongodb.net/mixDB?retryWrites=true",
+    "dbURL": "mongodb+srv://andreyxyz:andreyz@cluster0.n0bsv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
